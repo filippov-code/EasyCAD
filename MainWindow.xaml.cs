69694727e2390979c9de54c35d1a9d2a25aef7d0
Filippov Stepan <cooes.ef@gmail.com>
2022-11-25 22:58:24 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Defaults;
+using LiveCharts.Definitions.Series;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyCAD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SeriesCollection Series { get; set; } = new()
+        {
+            new LineSeries
+            {
+                Values = new ChartValues<int>{1, 2, 5, 4, 1, 7, 3, 7, 8, 4, 1}
+            },
+            new LineSeries
+            {
+                Values = new ChartValues<ObservablePoint>
+                {
+                    new ObservablePoint(0, 4),
+                    new ObservablePoint(1, 3),
+                    new ObservablePoint(3, 8),
+                    new (double.NaN, double.NaN){},
+                    new ObservablePoint(3, 5),
+                    new ObservablePoint(18, 6),
+                    new ObservablePoint(20, 12)
+                },
+                LineSmoothness = 0
+            }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+            DataContext = this;
+            Series.Add(new LineSeries
+            { 
+                Values = new ChartValues<ObservablePoint>()
+                
+                
+            });
+
+            for (double i = 0; i <= 20; i += 0.01d)
+            {
+                Series.Last().Values.Add(new ObservablePoint(Math.Round(i), Math.Sin(i)));
+            }
+
+            foreach (var series in Series)
+            {
+                series.Configuration = GetChartMapper(series);
+            }
+
+        }
+
+        private object GetChartMapper(ISeriesView series, float d)
+        {
+            return Mappers.Xy<ObservablePoint>()
+                                    .X((value, index) => index / d)
+                                    .Y((value, index) => value.Y)
+                                    .Fill((value, index) => index == 0 || index == series.Values.Count - 1 ? null : Brushes.Transparent)
+                                    .Stroke((value, index) => index == 0 || index == series.Values.Count - 1 ? Brushes.Red : Brushes.Transparent);
+
+        }
+    }
+}
